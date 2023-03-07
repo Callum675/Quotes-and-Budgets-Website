63a5b2dec83b03c726d237e3bf067583bfbbaf33
Callum Mcintosh <55438172+Callum675@users.noreply.github.com>
2023-03-07 23:34:48 +0000

    react frontend setup + login page

--- setup.sh
+++ setup.sh
@@ -7,9 +7,22 @@ RED="\e[31m"
 BLUE="\e[34m"
 UNSET="\e[0m"
 
+#install node/yarn
 
 
+cd frontend
+#install modules
 
+#start server
+
+cd server
+#create .env
+
+#install modules
+npm install
+
+#start server
+yarn start
 
 # All done
 echo "✅ Setup success"
