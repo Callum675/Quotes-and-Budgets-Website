ef31486fe6f7895b7dde1585a89b524e67d0bbbe
Callum Mcintosh <55438172+Callum675@users.noreply.github.com>
2023-03-28 23:21:20 +0100

    tailwind config changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,38 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        primary: "#0E8388",
-        "primary-dark": "#2E4F4F",
+        primary: '#0E8388',
+        'primary-dark': '#2E4F4F',
       },
       animation: {
-        loader: "loader 1s linear infinite",
+        loader: 'loader 1s linear infinite',
       },
       keyframes: {
         loader: {
-          "0%": { transform: "rotate(0) scale(1)" },
-          "50%": { transform: "rotate(180deg) scale(1.5)" },
-          "100%": { transform: "rotate(360deg) scale(1)" },
+          '0%': { transform: 'rotate(0) scale(1)' },
+          '50%': { transform: 'rotate(180deg) scale(1.5)' },
+          '100%': { transform: 'rotate(360deg) scale(1)' },
         },
       },
     },
+    fontSize: {
+      xxs: '.60rem',
+      xs: '.75rem',
+      sm: '.875rem',
+      tiny: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    },
   },
+  variants: {},
   plugins: [],
 };
